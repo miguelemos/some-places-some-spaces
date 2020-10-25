56d9b1a5a35ceea590063a7dcfa5eebf0ea568f0
Miguel Lemos <61155057+miguelemos@users.noreply.github.com>
2020-10-25 21:04:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ var NumberOfImages = 11 - 1;
 
 var img = new Array(NumberOfImages);
 
-img[0] = "Photos/2.jpg";
-img[1] = "Photos/3.jpg";
-img[2] = "Photos/4.jpg";
-img[3] = "Photos/5.jpg";
-img[4] = "Photos/6.jpg";
-img[5] = "Photos/7.jpg";
-img[6] = "Photos/8.jpg";
-img[7] = "Photos/9.jpg";
-img[8] = "Photos/10.jpg";
-img[9] = "Photos/12.jpg";
-img[10] = "Photos/13.jpg";
+img[1] = "Photos/2.jpg";
+img[2] = "Photos/3.jpg";
+img[3] = "Photos/4.jpg";
+img[4] = "Photos/5.jpg";
+img[5] = "Photos/6.jpg";
+img[6] = "Photos/7.jpg";
+img[7] = "Photos/8.jpg";
+img[8] = "Photos/9.jpg";
+img[9] = "Photos/10.jpg";
+img[10] = "Photos/12.jpg";
+img[11] = "Photos/13.jpg";
 
 // Array key of current image.
 var imgNumber = 0;
