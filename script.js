3291386652136f1040df7c04d676ed9f9661d220
Miguel Lemos <61155057+miguelemos@users.noreply.github.com>
2020-10-25 18:34:35 +0000

    Initial commit
    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// -1 because array members start from 0.
+var NumberOfImages = 11 - 1;
+
+var img = new Array(NumberOfImages);
+
+img[0] = "Photos/2.jpg";
+img[1] = "Photos/3.jpg";
+img[2] = "Photos/4.jpg";
+img[3] = "Photos/5.jpg";
+img[4] = "Photos/6.jpg";
+img[5] = "Photos/7.jpg";
+img[6] = "Photos/8.jpg";
+img[7] = "Photos/9.jpg";
+img[8] = "Photos/10.jpg";
+img[9] = "Photos/12.jpg";
+img[10] = "Photos/13.jpg";
+
+// Array key of current image.
+var imgNumber = 0;
+
+function NextImage() {
+    // Stop moving forward when we are out of images.
+    if (imgNumber < NumberOfImages) {
+        imgNumber++;
+        document.images["SwitchingImage"].src = img[imgNumber];
+    }
+}
+
+function PreviousImage() {
+    // Stop moving backward when we are out of images.
+    if (imgNumber != 0) {
+        imgNumber--;
+        document.images["SwitchingImage"].src = img[imgNumber];
+    }
+}
+
+
+function DisplayImage(id) {
+    imgNumber = id;
+    document.images["VCRImage"].src = "full_images/" + img[id];
+}
